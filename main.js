009716a9f2068213c3e069148bbb0e2bb600f8de
Noah Bazer <125102673+noahbazer@users.noreply.github.com>
2024-08-24 19:07:39 -0400

    added splash and base content, etc etc

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 window.onscroll = function () {
   scrollFunction();
 };
+
 function scrollFunction() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     document.getElementById('header').classList.add('scrolled');
@@ -9,3 +10,43 @@ function scrollFunction() {
     document.getElementById('header').classList.remove('scrolled');
   }
 }
+
+function disableScrolling() {
+  window.addEventListener('wheel', preventScroll, { passive: false });
+  window.addEventListener('touchmove', preventScroll, { passive: false });
+  window.addEventListener('keydown', preventScrollKeys, { passive: false });
+}
+
+function enableScrolling() {
+  window.removeEventListener('wheel', preventScroll, { passive: false });
+  window.removeEventListener('touchmove', preventScroll, { passive: false });
+  window.removeEventListener('keydown', preventScrollKeys, { passive: false });
+}
+
+function preventScroll(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
+}
+
+function preventScrollKeys(event) {
+  // Prevent scrolling with arrow keys, spacebar, and page up/down
+  if ([32, 33, 34, 35, 36, 37, 38, 39, 40].includes(event.keyCode)) {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  }
+}
+
+window.onload = function () {
+  // Scroll to the top on refresh
+  window.scrollTo(0, 0);
+
+  // Disable scrolling
+  disableScrolling();
+
+  // Enable scrolling after 4 seconds
+  setTimeout(function () {
+    enableScrolling();
+  }, 4000);
+};
